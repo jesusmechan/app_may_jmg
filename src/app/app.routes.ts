fe5a800d12279003ffa3b886c331fe0af4375c04
jesus.mechan <jesus.mechan@SESL-APS-4357>
2025-05-18 13:49:55 -0500

    agregando ruta activate-user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/components/auth/auth/auth.component';
 import { layoutRoutes } from './pages/components/principal/layout/app-layout.routes';
 import { AutenticadoGuard } from './guards/autenticado.guard';
+import { ActiveUserComponent } from './pages/components/active-user/active-user/active-user.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
+  { path: 'activate-user', component: ActiveUserComponent },
   { path: 'layout', children: layoutRoutes },
   {
     path: '**',
